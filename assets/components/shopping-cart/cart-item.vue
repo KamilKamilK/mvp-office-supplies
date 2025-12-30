e78003920d907e9a2953a67fcefaf575b8290113
Kamil Kaczmarek <kamil.kaczmarek88@gmail.com>
2025-12-31 00:11:34 +0100

    extend cart item

--- assets/components/shopping-cart/cart-item.vue
+++ assets/components/shopping-cart/cart-item.vue
@@ -1,8 +1,32 @@
 <template>
-    <div :class="[$style.component, 'row', 'p-3']">{{ item.product.name }} - Quantity: {{ item.quantity }}</div>
+    <div :class="[$style.component, 'row', 'p-3']">
+        <div class="col-2">
+            {{ item.product.name }}
+        </div>
+
+        <div class="col-1">
+            <span
+                class="color-square"
+                :style="{
+                    backgroundColor: `#${hexColor}`,
+                }"
+            />
+        </div>
+
+        <div class="col-3">
+            <input :value="item.quantity" type="number" class="form-control" min="1" @input="updateQuantity" />
+        </div>
+
+        <div class="col-3">${{ totalPrice }}</div>
+        <div class="col-3">
+            <button class="btn btn-danger">Remove</button>
+        </div>
+    </div>
 </template>
 
 <script>
+import formatPrice from '@/helpers/format-price.js';
+
 export default {
     name: 'ShoppingCartItem',
     props: {
@@ -11,15 +35,45 @@ export default {
             required: true,
         },
     },
+    emits: ['update-quantity'],
+    computed: {
+        hexColor() {
+            return this.item.color ? this.item.color.hexColor : 'fff';
+        },
+
+        totalPrice() {
+            return formatPrice(this.item.product.price * this.item.quantity);
+        },
+    },
+    methods: {
+        updateQuantity(event) {
+            this.$emit('update-quantity', {
+                productId: this.item.product['@id'],
+                colorId: this.item.color ? this.item.color['@id'] : null,
+                quantity: parseInt(event.target.value, 10),
+            });
+        },
+    },
 };
 </script>
 <style lang="scss" module>
 @use '~styles/variables/colors.scss' as colors;
 
-.component {
+.component :global {
     border-bottom: 1px solid colors.$light-component-border;
     a {
         color: colors.$light-component-link;
+    }
+
+    .color-square {
+        display: inline-block;
+        width: 20px;
+        height: 20px;
+        background-color: gray;
+    }
+
+    input {
+        width: 60px;
     }
 }
 </style>
--- assets/components/shopping-cart/index.vue
+++ assets/components/shopping-cart/index.vue
@@ -3,10 +3,27 @@
         <div v-if="items.length === 0">Your shopping cart is empty.</div>
     </div>
 
-    <shopping-cart-item v-for="(item, index) in items" :key="index" :item="item" />
+    <div v-if="items.length">
+        <div class="row p-3">
+            <div class="col-3">Item Name</div>
+            <div class="col-3">Quantity</div>
+            <div class="col-3">Price</div>
+            <div class="col-3" />
+        </div>
+        <shopping-cart-item
+            v-for="item in items"
+            :key="item.id"
+            :item="item"
+            @update-quantity="$emit('updateQuantity', $event)"
+        />
+        <div class="p-3">
+            Total: <strong>${{ totalPrice }}</strong>
+        </div>
+    </div>
 </template>
 <script>
 import ShoppingCartItem from '@/components/shopping-cart/cart-item.vue';
+import formatPrice from '@/helpers/format-price.js';
 
 export default {
     name: 'ShoppingCartList',
@@ -15,6 +32,12 @@ export default {
         items: {
             type: Array,
             required: true,
+        },
+    },
+    emits: ['updateQuantity'],
+    computed: {
+        totalPrice() {
+            return formatPrice(this.items.reduce((acc, item) => acc + item.product.price * item.quantity, 0));
         },
     },
 };
