7587ab14942e224e4d7db1a2f432938991f27d1f
Kamil Kaczmarek <kamil.kaczmarek88@gmail.com>
2026-01-07 17:36:52 +0100

    Improve lint formating

--- assets/components/shopping-cart/cart-sidebar.vue
+++ assets/components/shopping-cart/cart-sidebar.vue
@@ -1,23 +1,21 @@
 <template>
-  <div :class="[$style.component, 'p-3', 'mb-5']">
-    <h5 class="text-center">Featured Product!</h5>
+    <div :class="[$style.component, 'p-3', 'mb-5']">
+        <h5 class="text-center">Featured Product!</h5>
 
-    <img class="d-block mb-3" :src="featuredProduct.image" :alt="featuredProduct.name" />
+        <img class="d-block mb-3" :src="featuredProduct.image" :alt="featuredProduct.name" />
 
-    <div class="pt-3">
-      <h6>
-        {{ featuredProduct.name }} - ${{ price }}
-      </h6>
+        <div class="pt-3">
+            <h6>{{ featuredProduct.name }} - ${{ price }}</h6>
 
-      <cart-add-controls
-        :product="featuredProduct"
-        :add-to-cart-loading="false"
-        :add-to-cart-success="false"
-        :allow-add-to-cart="false"
-        add-button-text="View Product"
-      />
+            <cart-add-controls
+                :product="featuredProduct"
+                :add-to-cart-loading="false"
+                :add-to-cart-success="false"
+                :allow-add-to-cart="false"
+                add-button-text="View Product"
+            />
+        </div>
     </div>
-  </div>
 </template>
 
 <script>
@@ -25,16 +23,16 @@ import formatPrice from '@/helpers/format-price';
 import CartAddControls from '@/components/product-show/cart-add-controls.vue';
 
 export default {
-  name: 'ShoppingCartSidebar',
-  components: { CartAddControls },
-  props: {
-    featuredProduct: { type: Object, required: true },
-  },
-  computed: {
-    price() {
-      return formatPrice(this.featuredProduct.price);
+    name: 'ShoppingCartSidebar',
+    components: { CartAddControls },
+    props: {
+        featuredProduct: { type: Object, required: true },
     },
-  },
+    computed: {
+        price() {
+            return formatPrice(this.featuredProduct.price);
+        },
+    },
 };
 </script>
 
@@ -42,23 +40,23 @@ export default {
 @use '~styles/components/light-component' as lc;
 
 .component {
-  @include lc.light-component;
+    @include lc.light-component;
 
-  img {
-    max-width: 100%;
-    border-radius: 8px;
-  }
+    img {
+        max-width: 100%;
+        border-radius: 8px;
+    }
 
-  h5 {
-    margin-bottom: 1rem;
-  }
+    h5 {
+        margin-bottom: 1rem;
+    }
 
-  h6 {
-    margin-bottom: 0.5rem;
-  }
+    h6 {
+        margin-bottom: 0.5rem;
+    }
 
-  input {
-    width: 60px;
-  }
+    input {
+        width: 60px;
+    }
 }
 </style>
--- assets/pages/shopping-cart.vue
+++ assets/pages/shopping-cart.vue
@@ -1,29 +1,26 @@
 <template>
-  <div :class="[$style.component, 'container-fluid']">
-    <div class="row">
-      <!-- Sidebar Featured Product -->
-      <div class="col-xs-12 col-lg-3 mb-3">
-        <shopping-cart-sidebar
-          v-if="featuredProduct"
-          :featured-product="featuredProduct"
-        />
-      </div>
+    <div :class="[$style.component, 'container-fluid']">
+        <div class="row">
+            <!-- Sidebar Featured Product -->
+            <div class="col-xs-12 col-lg-3 mb-3">
+                <shopping-cart-sidebar v-if="featuredProduct" :featured-product="featuredProduct" />
+            </div>
 
-      <!-- Lista koszyka -->
-      <div class="col-xs-12 col-lg-9">
-        <title-component text="Shopping Cart" />
-        <div class="content p-3">
-          <loading v-show="completeCart === null" />
-          <shopping-cart-list
-            v-if="completeCart"
-            :items="completeCart.items"
-            @update-quantity="updateQuantity"
-            @remove-from-cart="removeProductFromCart($event.productId, $event.colorId)"
-          />
+            <!-- Lista koszyka -->
+            <div class="col-xs-12 col-lg-9">
+                <title-component text="Shopping Cart" />
+                <div class="content p-3">
+                    <loading v-show="completeCart === null" />
+                    <shopping-cart-list
+                        v-if="completeCart"
+                        :items="completeCart.items"
+                        @update-quantity="updateQuantity"
+                        @remove-from-cart="removeProductFromCart($event.productId, $event.colorId)"
+                    />
+                </div>
+            </div>
         </div>
-      </div>
     </div>
-  </div>
 </template>
 
 <script>
@@ -37,57 +34,57 @@ import { fetchColors } from '@/services/colors-service';
 import { updateCartItemQuantity } from '@/services/cart-service';
 
 export default {
-  name: 'ShoppingCart',
-  components: {
-    TitleComponent,
-    Loading,
-    ShoppingCartList,
-    ShoppingCartSidebar,
-  },
-  mixins: [ShoppingCartMixin],
-  data() {
-    return {
-      products: null,
-      colors: null,
-      featuredProduct: null,
-    };
-  },
-  computed: {
-    completeCart() {
-      if (!this.cart || !this.products || !this.colors) return null;
-
-      const completeItems = this.cart.items.map(cartItem => {
-        const product = this.products.find(p => p['@id'] === cartItem.product);
-        const color = this.colors.find(c => c['@id'] === cartItem.color) || null;
+    name: 'ShoppingCart',
+    components: {
+        TitleComponent,
+        Loading,
+        ShoppingCartList,
+        ShoppingCartSidebar,
+    },
+    mixins: [ShoppingCartMixin],
+    data() {
         return {
-          id: `${cartItem.product}_${cartItem.color ?? 'none'}`,
-          product,
-          color,
-          quantity: cartItem.quantity,
+            products: null,
+            colors: null,
+            featuredProduct: null,
         };
-      });
+    },
+    computed: {
+        completeCart() {
+            if (!this.cart || !this.products || !this.colors) return null;
 
-      return { items: completeItems };
+            const completeItems = this.cart.items.map(cartItem => {
+                const product = this.products.find(p => p['@id'] === cartItem.product);
+                const color = this.colors.find(c => c['@id'] === cartItem.color) || null;
+                return {
+                    id: `${cartItem.product}_${cartItem.color ?? 'none'}`,
+                    product,
+                    color,
+                    quantity: cartItem.quantity,
+                };
+            });
+
+            return { items: completeItems };
+        },
     },
-  },
-  async created() {
-    this.loadFeaturedProduct();
-    this.colors = (await fetchColors()).data.member || (await fetchColors()).data['hydra:member'];
-  },
-  watch: {
-    async cart() {
-      this.loadProducts();
+    watch: {
+        async cart() {
+            this.loadProducts();
+        },
     },
-  },
-  methods: {
-    async loadProducts() {
-      const productIds = this.cart.items.map(item => item.product);
-      const response = await fetchProductsById(productIds);
-      this.products = response.data.member || response.data['hydra:member'];
+    async created() {
+        this.loadFeaturedProduct();
+        this.colors = (await fetchColors()).data.member || (await fetchColors()).data['hydra:member'];
     },
-    updateQuantity({ productId, colorId, quantity }) {
-      updateCartItemQuantity(this.cart, productId, colorId, quantity);
-    },
+    methods: {
+        async loadProducts() {
+            const productIds = this.cart.items.map(item => item.product);
+            const response = await fetchProductsById(productIds);
+            this.products = response.data.member || response.data['hydra:member'];
+        },
+        updateQuantity({ productId, colorId, quantity }) {
+            updateCartItemQuantity(this.cart, productId, colorId, quantity);
+        },
         async loadFeaturedProduct() {
             const featuredProducts =
                 (await fetchFeaturedProducts()).data.member || (await fetchFeaturedProducts()).data['hydra:member'];
